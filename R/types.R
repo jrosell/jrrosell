dc7dfa2cc979e7a1f7b23f3f8c9c6e23dc8d416d
Jordi Rosell <jroselln@gmail.com>
2023-08-04 09:49:21 +0200

    comments

--- R/types.R
+++ R/types.R
@@ -6,6 +6,8 @@
 #' @param x A numeric vetor.
 #'
 #' @details Get the bit representation of a double number
+#' Using rev() ensures that the bit order is correct, and the binary representation aligns with the usual convention of having the MSB first and the LSB last.
+#' This is because numToBits() returns the bits in the reverse order, and without rev(), we end up with the LSB first and the MSB last.
 #'
 #' @examples
 #' 0.1 + 0.2 == 0.3
