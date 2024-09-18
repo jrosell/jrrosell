887d23ec84e02ff13ad5661f964c455949373d20
Jordi Rosell <jroselln@gmail.com>
2024-09-18 21:40:02 +0200

    use alternate font if it fails loading roboto

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,24 @@
+utils::globalVariables(c(
+  "ROBOTO_CONDENSED", "ROBOTO_BLACK"
+))
+
 .onLoad <- function(libname, pkgname) {
   # extrafont::font_import()
-  if (rlang::is_installed("extrafont")) {
-    extrafont::loadfonts(device = "all", quiet = TRUE)
+  if (!rlang::is_installed("extrafont")) stop("extrafont package is required")
+
+  ROBOTO_CONDENSED <<- "Roboto Condensed"
+  ROBOTO_BLACK <<- "Roboto Black"
+  if (.Platform$OS.type == "windows" && tools::find_gs_cmd() != "") {
+    warning("'ghostscript' is required but not found")
+     ROBOTO_CONDENSED <<- "Arial"
+     ROBOTO_BLACK <<- "Arial Black"
+  }    
+  n_roboto <-
+    extrafont::fonts() |>
+    purrr::keep(\(x) stringr::str_detect(x, "Roboto")) |>
+    length()
+  if (n_roboto == 0) {
+    extrafont::font_import(prompt = FALSE, pattern = "Roboto")
   }
+  extrafont::loadfonts(device = "all", quiet = TRUE)
 }
